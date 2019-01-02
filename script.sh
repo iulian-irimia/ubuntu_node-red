983a81159fc4345ead6cb87e90eb3b0d1d417cfc
iiulian <iulian.v.irimia@gmail.com>
2019-01-02 17:23:31 +0200

    Some cleanup, done

--- script.sh
+++ script.sh
@@ -4,11 +4,6 @@ apt-get upgrade -y
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 
-#if ! which node >/dev/null; then
-#    echo "node command not found... synliked created"
-#    ln -s /usr/bin/nodejs /usr/bin/node
-#fi
-
 npm install -g --unsafe-perm node-red
 
 echo "==================================="
